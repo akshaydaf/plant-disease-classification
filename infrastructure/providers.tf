d6f4b47d776c98dcce671521594264f84afc8d26
Akshay Daftardar <akshaydaf@gmail.com>
2025-06-29 17:21:45 -0400

    feat: undo comment of aws_eks_cluster_auth

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -14,6 +14,6 @@ provider "helm" {
 #   name = module.eks.cluster_name
 # }
 
-# data "aws_eks_cluster_auth" "cluster" {
-#   name = module.eks.cluster_name
-# }
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
