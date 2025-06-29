24c81da60de29f0a29609878b31cfc4f200c76eb
Akshay Daftardar <akshaydaf@gmail.com>
2025-06-29 17:20:26 -0400

    feat: comment out data block

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -10,10 +10,10 @@ provider "helm" {
   }
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
+# data "aws_eks_cluster" "cluster" {
+#   name = module.eks.cluster_name
+# }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = module.eks.cluster_name
+# }
